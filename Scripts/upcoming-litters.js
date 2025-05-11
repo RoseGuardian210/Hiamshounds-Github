3d7cecfc7968c21a532a6d04296a18810ce6615a
RoseGuardian210 <martynas.lutinskas@gmail.com>
2025-05-11 20:39:34 +0100

    Add files via upload

--- Scripts/upcoming-litters.js
+++ Scripts/upcoming-litters.js
@@ -11,3 +11,24 @@ $(document).ready(function() {
         $('img').removeClass('enlarged'); // Remove from all
     });
 });
+
+// no litters button movement
+
+function moveButton() {
+    const buttonLink = document.querySelector('.EnquireBTN')?.parentElement;
+    const nolittersContainer = document.querySelector('.no-litters');
+    const imgContainer = document.querySelector('.noLitters-imgContainer');
+
+    if (!buttonLink || !nolittersContainer || !imgContainer) return;
+
+    const isMobileOrTablet = window.matchMedia("(max-width: 1100px)").matches;
+
+    if (isMobileOrTablet && buttonLink.parentElement === imgContainer) {
+      nolittersContainer.insertBefore(buttonLink, nolittersContainer.firstChild);
+    } else if (!isMobileOrTablet && buttonLink.parentElement === nolittersContainer) {
+      imgContainer.appendChild(buttonLink);
+    }
+  }
+
+  window.addEventListener('DOMContentLoaded', moveButton);
+  window.addEventListener('resize', moveButton);
